a1110c3cb39282fa56f923045d273884ceb4c14c
AliAlsed <alialsed1996@yahoo.com>
2021-06-11 16:10:46 +0300

    upload to heroku11

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,8 +18,9 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', admin.site.urls),
     path('task/', include('task.urls'), name='task'),
 
     path('', auth_views.LoginView.as_view(
